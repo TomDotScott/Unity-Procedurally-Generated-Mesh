252f99ac700166228a997d775ed1462ac8be6dfe
tomdotscott <tom.scott45456@gmail.com>
2020-06-06 15:04:03 +0100

    Perlin Noise
    The height of each quad was changed using Mathf's PerlinNoise function

--- Procedural Terrain/Assets/Scripts/MeshGenerator.cs
+++ Procedural Terrain/Assets/Scripts/MeshGenerator.cs
@@ -33,7 +33,8 @@ public class MeshGenerator : MonoBehaviour
         {
             for(int x = 0; x <= xSize; x++)
             {
-                vertices[index] = new Vector3(x, 0, z);
+                float y = Mathf.PerlinNoise(x * 0.3f, z * 0.3f) * 2;
+                vertices[index] = new Vector3(x, y, z);
                 index++;
             }
         }
